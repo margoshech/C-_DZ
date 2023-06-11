3f9a775e708db82b48c3db73cff788d4de60d908
Margoshech <margoshech@gmail.com>
2023-06-11 23:52:13 +0300

    DZ9 ITOGOVAYA

---
+++ C#DZ9_ITOG/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+// string PrintNum(int num)
+// {
+//     if (num == 1)
+//     {
+//         return "1";
+//     }
+
+//     return num.ToString() + "," + PrintNum(num - 1);
+// }
+
+// Console.Clear();
+// System.Console.WriteLine("Введите целое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Натуральные числа в промежутке от {num} до 1: ");
+// System.Console.WriteLine(PrintNum(num));
+
+
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+// int Summa(int current, int finish)
+// {
+//     int result = current;
+//     if (current < finish)
+//     {
+//         result += Summa(current + 1, finish);
+//     }
+//     return result;
+// }
+
+
+// Console.Clear();
+// System.Console.WriteLine("Введите целое число M");
+// int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите целое число N");
+// int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Сумма от {m} до {n}: ");
+// System.Console.WriteLine(Summa(m, n));
+
+
+
+
+
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.Write("Введите значение M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int akkerman(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     else if (n == 0) return akkerman(m - 1, 1);
+//     else return akkerman(m - 1, akkerman(m, n - 1));
+// }
+
+// Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
