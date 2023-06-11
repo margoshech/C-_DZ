18b5ed805ed025cd1d5a8f45f56f70cf6ddb6743
Margoshech <margoshech@gmail.com>
2023-06-12 00:06:57 +0300

    DZ9 ITOGOVAYA

--- C#DZ9_ITOG/Program.cs
+++ C#DZ9_ITOG/Program.cs
@@ -60,16 +60,16 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-// Console.Write("Введите значение M: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите значение N: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// int akkerman(int m, int n)
-// {
-//     if (m == 0) return n + 1;
-//     else if (n == 0) return akkerman(m - 1, 1);
-//     else return akkerman(m - 1, akkerman(m, n - 1));
-// }
+int akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return akkerman(m - 1, 1);
+    else return akkerman(m - 1, akkerman(m, n - 1));
+}
 
-// Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
+Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
